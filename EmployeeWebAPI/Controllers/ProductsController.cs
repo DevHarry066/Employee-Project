6526789a8b2efdac1ed0591231be566e0e7c8921
DevHarry066 <sharmahariom066@gmail.com>
2021-08-24 17:04:05 +0530

    Put Method

--- EmployeeWebAPI/Controllers/ProductsController.cs
+++ EmployeeWebAPI/Controllers/ProductsController.cs
@@ -30,7 +30,9 @@ namespace EmployeeWebAPI.Controllers
 
             var filePath = Path.Combine("wwwroot", guid + ".jpg");
 
-            if(product.Image!=null)
+            // image URL-https://localhost:44389//261ed257-01e6-4864-87c4-cc77fd7395d0.jpg
+            
+            if (product.Image!=null)
             {
                 var fileStream = new FileStream(filePath, FileMode.Create);
                 product.Image.CopyTo(fileStream);
@@ -41,5 +43,50 @@ namespace EmployeeWebAPI.Controllers
 
             return StatusCode(StatusCodes.Status201Created);
         }
+
+        [HttpPut({"id"})]
+
+        public IActionResult Put(int id, [FromForm] Product product)
+        {
+        var p = _dbContext.Products.Find(id);
+        if(p==null)
+        {
+            return NotFound("not found");
+        }
+
+        else
+        {
+            var guid = Guid.NewGuid();
+            var filePath=Path.Combine("wwwroot",guid+".jpg");
+            if(product.Image!=null)
+            {
+                var fileStream = new FileStream(filePath, FileMode.Create);
+                product.Image.CopyTo(fileStream);
+                product.ImageUrl = filePath.Remove(0, 7);
+
+            }
+
+            p.ProductName = product.ProductName;
+            p.Rating = product.Rating;
+            p.Categories = product.Categories;
+            p.SKU = product.SKU;
+            p.Discount = product.Discount;
+            p.Percentage = product.Percentage;
+            p.Details = product.Details;
+            p.Tag = product.Tag;
+            p.Price = product.Price;
+            p.Stock = product.Stock;
+
+            _dbContext.SaveChanges();
+            return OK("Updated Succeessfully");
+        }
+
+        }
+
+
+
+
+
+
     }
 }
---
+++ EmployeeWebAPI/Migrations/20210823121114_EditProduct.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeWebAPI.Migrations
+{
+    public partial class EditProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Categories",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Discount",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Percentage",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tag",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Categories",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Discount",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Percentage",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Tag",
+                table: "Products");
+        }
+    }
+}
